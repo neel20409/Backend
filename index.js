3353ae195e8245f6dcb4d2cb65157e3059460207
neel20409 <bhattneel2004@gmail.com>
2025-10-04 17:43:47 +0530

    first comit

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("Neel Bhatt");
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+app.get('/twitter', (req, res) => {
+  res.send('<h1>Neel bhatt Twittter Page</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
